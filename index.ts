cd5079827372bd35ced0ed593ca7a6c87ebbdba7
Nayan Dey <ndeynayan123@gmail.com>
2026-07-12 01:58:09 +0530

    feat: connect MongoDB and add product creation API

---
+++ index.ts
@@ -0,0 +1,95 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+    throw new Error("MONGODB_URI is missing");
+}
+
+
+
+const app: Application = express();
+const port = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req: Request, res: Response) => {
+    res.send("ResellHub Server is Running...");
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const database = client.db(process.env.DB_NAME);
+
+        const productsCollection = database.collection("products");
+        const usersCollection = database.collection("users");
+
+
+        // insert products
+        app.post("/products", async (req: Request, res: Response) => {
+            try {
+                const product = req.body;
+
+                const result = await productsCollection.insertOne(product);
+
+                res.status(201).send({
+                    success: true,
+                    message: "Product added successfully",
+                    insertedId: result.insertedId,
+                });
+
+            } catch (error) {
+                console.error(error);
+
+                res.status(500).send({
+                    success: false,
+                    message: "Failed to add product",
+                });
+            }
+        });
+
+       
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
